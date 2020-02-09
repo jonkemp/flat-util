9544e191007f72cd08c299bb5e7768778aab7985
Jonathan Kemp <kempdogg@gmail.com>
2020-02-08 23:50:09 -0500

    Update build script.

--- build.js
+++ build.js
@@ -1,3 +1,37 @@
 const umdify = require('umd-util');
 
-umdify('flatten', 'umd', 'index.js', true);
+const templateSource = `(function(f) {
+	if (typeof exports === "object" && typeof module !== "undefined") {
+        module.exports = f()
+    } else if (typeof define === "function" && define.amd) {
+        define([], f)
+    } else {
+        var g;
+        if (typeof window !== "undefined") {
+            g = window
+        } else if (typeof global !== "undefined") {
+            g = global
+        } else if (typeof self !== "undefined") {
+            g = self
+        } else {
+            g = this
+        }
+        g.<%= namespace %> = f()
+    }
+})(function() {
+    var define, module, exports;
+    module = {
+        exports: (exports = {})
+    };
+
+    <%= contents %>
+
+    return module.exports;
+});`;
+
+umdify('index.js', {
+	templateSource,
+	namespace() {
+		return 'flatten';
+	}
+});
