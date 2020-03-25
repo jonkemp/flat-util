c1fdbace74ab803d9877468354be31f951bbf496
Jonathan Kemp <kempdogg@gmail.com>
2020-03-24 23:31:59 -0400

    Add package-utils.

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const isArrayLike = require('./lib/is-array-like');
-
-const isArguments = obj => toString.call(obj) === '[object Arguments]';
+const { isArrayLike, isArguments } = require('@jonkemp/package-utils');
+const forEach = require('for-each-plus');
 
 const flatten = (input, shallow, strict, output = []) => {
 	let idx = output.length;
-	input.forEach(value => {
+
+	forEach(input, value => {
 		if (isArrayLike(value) && (Array.isArray(value) || isArguments(value))) {
 			if (shallow) {
 				let j = 0;
 				const len = value.length;
+
 				while (j < len) output[idx++] = value[j++];
 			} else {
 				flatten(value, shallow, strict, output);
@@ -18,6 +19,7 @@ const flatten = (input, shallow, strict, output = []) => {
 			output[idx++] = value;
 		}
 	});
+
 	return output;
 };
 
