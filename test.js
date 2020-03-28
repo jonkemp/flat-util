ee9a5e26ae132034b9fb002eb332c877d7055f7a
Jonathan Kemp <kempdogg@gmail.com>
2020-03-28 00:01:24 -0400

    Add karma for running tests.

--- test.js
+++ test.js
@@ -1,5 +1,8 @@
-const assert = require('assert');
-const flat = require('./');
+/* global flatten */
+const { assert } = chai;
+
+mocha.setup('bdd');
+mocha.checkLeaks();
 
 const range = (start, stop, step) => {
 	if (stop == null) {
@@ -20,37 +23,37 @@ const range = (start, stop, step) => {
 	return range;
 };
 
-describe('flat', () => {
+describe('flatten', () => {
 	it('supports null', () => {
-		assert.deepEqual(flat(null), []);
+		assert.deepEqual(flatten(null), []);
 	});
 
 	it('supports undefined', () => {
-		assert.deepEqual(flat(void 0), []);
+		assert.deepEqual(flatten(void 0), []);
 	});
 
 	it('supports empty arrays', () => {
-		assert.deepEqual(flat([[], [[]], []]), []);
+		assert.deepEqual(flatten([[], [[]], []]), []);
 	});
 
 	it('can shallowly flatten empty arrays', () => {
-		assert.deepEqual(flat([[], [[]], []], true), [[]]);
+		assert.deepEqual(flatten([[], [[]], []], true), [[]]);
 	});
 
 	it('can flatten nested arrays', () => {
 		const list = [1, [2], [3, [[[4]]]]];
 
-		assert.deepEqual(flat(list), [1, 2, 3, 4]);
+		assert.deepEqual(flatten(list), [1, 2, 3, 4]);
 	});
 
 	it('can shallowly flatten nested arrays', () => {
 		const list = [1, [2], [3, [[[4]]]]];
 
-		assert.deepEqual(flat(list, true), [1, 2, 3, [[[4]]]]);
+		assert.deepEqual(flatten(list, true), [1, 2, 3, [[[4]]]]);
 	});
 
 	it('works on an arguments object', () => {
-		const result = (function(...args) { return flat(args); }(1, [2], [3, [[[4]]]]));
+		const result = (function(){ return flatten(arguments); }(1, [2], [3, [[[4]]]]));
 
 		assert.deepEqual(result, [1, 2, 3, 4]);
 	});
@@ -58,40 +61,42 @@ describe('flat', () => {
 	it('can shallowly flatten arrays containing only other arrays', () => {
 		const list = [[1], [2], [3], [[4]]];
 
-		assert.deepEqual(flat(list, true), [1, 2, 3, [4]]);
+		assert.deepEqual(flatten(list, true), [1, 2, 3, [4]]);
 	});
 
 	it('can flatten medium length arrays', () => {
-		assert.strictEqual(flat([range(10), range(10), 5, 1, 3], true).length, 23);
+		assert.strictEqual(flatten([range(10), range(10), 5, 1, 3], true).length, 23);
 	});
 
 	it('can shallowly flatten medium length arrays', () => {
-		assert.strictEqual(flat([range(10), range(10), 5, 1, 3]).length, 23);
+		assert.strictEqual(flatten([range(10), range(10), 5, 1, 3]).length, 23);
 	});
 
 	it('can flatten array with nulls of size n', () => {
-		assert.strictEqual(flat([new Array(10)]).length, 10);
+		assert.strictEqual(flatten([new Array(10)]).length, 10);
 	});
 
 	it('can handle massive arrays', () => {
-		assert.strictEqual(flat([new Array(1000000), range(56000), 5, 1, 3]).length, 1056003);
+		assert.strictEqual(flatten([new Array(1000000), range(56000), 5, 1, 3]).length, 1056003);
 	});
 
 	it('can handle massive arrays in shallow mode', () => {
-		assert.strictEqual(flat([new Array(1000000), range(56000), 5, 1, 3], true).length, 1056003);
+		assert.strictEqual(flatten([new Array(1000000), range(56000), 5, 1, 3], true).length, 1056003);
 	});
 
 	it('can handle very deep arrays', () => {
 		let x = range(100000);
 
 		for (let i = 0; i < 1000; i++) x = [x];
-		assert.deepEqual(flat(x), range(100000));
+		assert.deepEqual(flatten(x), range(100000));
 	});
 
 	it('can handle very deep arrays in shallow mode', () => {
 		let x = range(100000);
 
 		for (let i = 0; i < 1000; i++) x = [x];
-		assert.deepEqual(flat(x, true), x[0]);
+		assert.deepEqual(flatten(x, true), x[0]);
 	});
 });
+
+mocha.run();
